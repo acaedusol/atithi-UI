ddb5cb359b29742c7246cf90f49dd887bd26d9fe
Shashank Mishra <shashankmis808@gmail.com>
2024-05-09 22:07:58 +0530

    Added order manipulation ui

---
+++ src/Models/Order.ts
@@ -0,0 +1,9 @@
+export interface OrderItem {
+  menuItemId: string; // Unique identifier for the menu item
+  quantity: number; // Quantity of the item ordered
+}
+
+export interface Order {
+  roomId: number; // Room identifier
+  orderItems: OrderItem[]; // List of order items
+}
--- src/app/Category/category-list/category-list.component.ts
+++ src/app/Category/category-list/category-list.component.ts
@@ -1,6 +1,7 @@
 // src/app/components/category-list/category-list.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Category, MenuItem } from '../../../Models/Category';
+import { OrderItem } from '../../../Models/Order';
 import { CategoryMenuService } from '../../service/CategoryMenu/categorymenu.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
@@ -16,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
 export class CategoryListComponent implements OnInit {
   categories: Category[] = []; // Array to hold fetched categories
   selectedCategory: Category | undefined;
-
+  orderItems: OrderItem[] = [];
   constructor(private categoryService: CategoryMenuService) {}
 
   ngOnInit(): void {
@@ -34,5 +35,64 @@ export class CategoryListComponent implements OnInit {
   // Get menu items for the selected category
   getMenuItemsForSelectedCategory(): MenuItem[] {
     return this.selectedCategory ? this.selectedCategory.items : [];
+  }
+
+  selectItem(item: MenuItem): void {
+    const existingItem = this.orderItems.find(
+      (orderItem) => orderItem.menuItemId === item.menuId
+    );
+
+    if (existingItem) {
+      existingItem.quantity += 1; // Increase the quantity
+    } else {
+      // Add the item with a quantity of 1
+      this.orderItems.push({ menuItemId: item.menuId, quantity: 1 });
+    }
+  }
+
+  // Increase the quantity of an order item
+  increaseItem(item: MenuItem): void {
+    const existingItem = this.orderItems.find(
+      (orderItem) => orderItem.menuItemId === item.menuId
+    );
+
+    if (existingItem) {
+      existingItem.quantity += 1; // Increase by 1
+    }
+  }
+
+  // Reduce the quantity of an order item
+  reduceItem(item: MenuItem): void {
+    const existingItem = this.orderItems.find(
+      (orderItem) => orderItem.menuItemId === item.menuId
+    );
+
+    if (existingItem && existingItem.quantity > 1) {
+      existingItem.quantity -= 1; // Decrease by 1
+    } else if (existingItem && existingItem.quantity === 1) {
+      this.orderItems = this.orderItems.filter(
+        (orderItem) => orderItem.menuItemId !== item.menuId
+      ); // Remove the item if quantity is 0
+    }
+  }
+
+  // Determine whether the "Add" button should be shown
+  showAddButton(item: MenuItem): boolean {
+    return !this.orderItems.some(
+      (orderItem) => orderItem.menuItemId === item.menuId
+    );
+  }
+
+  // Determine whether the "+" and "-" buttons should be shown
+  showIncrementDecrementButtons(item: MenuItem): boolean {
+    return !this.showAddButton(item); // Shown when "Add" is not shown
+  }
+
+  getItemQuantity(item: MenuItem): number {
+    const existingItem = this.orderItems.find(
+      (orderItem) => orderItem.menuItemId === item.menuId
+    );
+
+    return existingItem ? existingItem.quantity : 0; // Return 0 if not found
   }
 }
