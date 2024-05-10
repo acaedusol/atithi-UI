adce4cc0189f59d28816f13237abc332ffda7bd7
Shashank Mishra <shashankmis808@gmail.com>
2024-05-11 03:23:30 +0530

    Fix header issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  showHeader = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // List of routes where the header should be hidden
+        const routesWithoutHeader = ['/home'];
+        this.showHeader = routesWithoutHeader.includes(this.router.url);
+      }
+    });
+  }
+}
